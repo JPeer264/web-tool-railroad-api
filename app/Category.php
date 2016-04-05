8ecdd3a8af8dd60c7c05dd443fb7667137ea1724
Hannah93 <Hannelore.hauquier@hotmail.com>
2016-04-05 23:16:45 +0200

    category model

--- app/Category.php
+++ app/Category.php
@@ -9,7 +9,25 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $fillable = [
-
+        'title', 'description',
     ];
 
+    // LINK THIS MODEL TO OUR DATABASE TABLE ---------------------------------
+    // since the plural of user isnt what we named our database table we have to define it
+    protected $table = 'category';
+
+    // DEFINE RELATIONSHIPS --------------------------------------------------
+    // define a many to many relationship
+    // also call the linking table
+    public function company() {
+        return $this->belongsToMany('Company', 'Category_Company', 'category_id', 'company_id');
+    }
+
+    public function job() {
+        return $this->belongsToMany('Job', 'Category_Job', 'category_id', 'job_id');
+    }
+
+    public function topic() {
+        return $this->hasMany('Topic');
+    }
 }
