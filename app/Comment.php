87d79376b9ef629695f3e87181a529a7f3a9a2e2
Hannah93 <Hannelore.hauquier@hotmail.com>
2016-04-05 23:21:36 +0200

    comment model

--- app/Comment.php
+++ app/Comment.php
@@ -9,7 +9,21 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     protected $fillable = [
-
+        'content',
     ];
+
+    // LINK THIS MODEL TO OUR DATABASE TABLE ---------------------------------
+    // since the plural of user isnt what we named our database table we have to define it
+    protected $table = 'comment';
+
+    // DEFINE RELATIONSHIPS --------------------------------------------------
+
+    public function topic() {
+        return $this->belongsTo('Topic');
+    }
+
+    public function user() {
+        return $this->belongsTo('User');
+    }
 
 }
