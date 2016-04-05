cdf20ae8ab9ec17ff4ec3937da52c078f5b7fab8
Hannah93 <Hannelore.hauquier@hotmail.com>
2016-04-05 23:44:43 +0200

    type model

--- app/Type.php
+++ app/Type.php
@@ -9,7 +9,19 @@ use Illuminate\Database\Eloquent\Model;
 class Type extends Model
 {
     protected $fillable = [
-
+        'title', 'description',
     ];
+
+    // LINK THIS MODEL TO OUR DATABASE TABLE ---------------------------------
+    // since the plural of user isnt what we named our database table we have to define it
+    protected $table = 'type';
+
+
+    // DEFINE RELATIONSHIPS --------------------------------------------------
+
+    public function topic() {
+        return $this->hasMany('Topic');
+    }
+
 
 }
