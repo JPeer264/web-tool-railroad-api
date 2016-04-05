633058fb1a347304bcf2ce8f34bc17aa867c95ba
Hannah93 <Hannelore.hauquier@hotmail.com>
2016-04-05 23:37:18 +0200

    job model

--- app/Job.php
+++ app/Job.php
@@ -9,7 +9,23 @@ use Illuminate\Database\Eloquent\Model;
 class Job extends Model
 {
     protected $fillable = [
-
+        'title', 'description',
     ];
 
+    // LINK THIS MODEL TO OUR DATABASE TABLE ---------------------------------
+    // since the plural of user isnt what we named our database table we have to define it
+    protected $table = 'job';
+
+    // DEFINE RELATIONSHIPS --------------------------------------------------
+    public function category() {
+        return $this->belongsToMany('Category', 'Category_Job', 'category_id', 'job_id');
+    }
+
+    public function topic() {
+        return $this->belongsToMany('Topic', 'Topic_Job', 'topic_id', 'job_id');
+    }
+
+    public function user() {
+        return $this->hasMany('User');
+    }
 }
