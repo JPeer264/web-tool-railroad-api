015a011b0ff3340fe2f86512a9b6ac59a7a28600
Hannah93 <Hannelore.hauquier@hotmail.com>
2016-04-05 23:29:21 +0200

    company model

--- app/Company.php
+++ app/Company.php
@@ -9,7 +9,28 @@ use Illuminate\Database\Eloquent\Model;
 class Company extends Model
 {
     protected $fillable = [
+        'administrator', 'name', 'logo_alt', 'logo_location',
+        'country', 'city', 'address', 'phonenumber', 'email',
 
     ];
 
+    // LINK THIS MODEL TO OUR DATABASE TABLE ---------------------------------
+    // since the plural of user isnt what we named our database table we have to define it
+    protected $table = 'company';
+
+
+    // DEFINE RELATIONSHIPS --------------------------------------------------
+    // define a many to many relationship
+    // also call the linking table
+    public function category() {
+        return $this->belongsToMany('Category', 'Category_Company', 'category_id', 'company_id');
+    }
+
+    public function topic() {
+        return $this->belongsToMany('Topic', 'Topic_Company', 'topic_id', 'company_id');
+    }
+
+    public function user() {
+        return $this->hasMany('User');
+    }
 }
