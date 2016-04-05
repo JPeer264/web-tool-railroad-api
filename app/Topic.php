5cda714cbf566c6f5593bd537ca8d7a55e2c531a
Hannah93 <Hannelore.hauquier@hotmail.com>
2016-04-05 23:42:29 +0200

    topic model

--- app/Topic.php
+++ app/Topic.php
@@ -9,7 +9,36 @@ use Illuminate\Database\Eloquent\Model;
 class Topic extends Model
 {
     protected $fillable = [
-
+        'title', 'description',
     ];
 
+    // LINK THIS MODEL TO OUR DATABASE TABLE ---------------------------------
+    // since the plural of user isnt what we named our database table we have to define it
+    protected $table = 'topic';
+
+    // DEFINE RELATIONSHIPS --------------------------------------------------
+    public function company() {
+        return $this->belongsToMany('Company', 'Topic_Company', 'topic_id', 'company_id');
+    }
+
+    public function job() {
+        return $this->belongsToMany('Job', 'Topic_Job', 'top_id', 'job_id');
+    }
+
+    public function comment() {
+        return $this->hasMany('Comment');
+    }
+
+
+    public function category() {
+        return $this->belongsTo('Category');
+    }
+
+    public function user() {
+        return $this->belongsTo('User');
+    }
+
+    public function type() {
+        return $this->belongsTo('Type');
+    }
 }
