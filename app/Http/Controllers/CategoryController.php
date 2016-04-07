8acb6e12a6eaac76d20624b4a1a627fdfc1f2745
jpeer264 <jan.oster94@gmail.com>
2016-04-08 01:44:51 +0200

    added filter and category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Category;
 use App\Http\Controllers\Filter;
+use App\Job;
 
 class CategoryController extends Controller
 {
@@ -37,33 +38,62 @@ class CategoryController extends Controller
      */
     public function getAll(Request $request) {
         $categories = Category::with('topic', 'job', 'company')->get();
-        $params = $request->all();
-        $filter = new Filter($categories);
+        $filter = new Filter($categories, $request->all());
 
         // filter by given parameters 
-        $companyFilter  = $filter
-            ->byParameters($params, 'company')
-            ->byParameters($params, 'job');
+        $filtered  = $filter
+            ->byParameters('company')
+            ->byParameters('job');
 
-        return response()->json($companyFilter->getArray()->toArray());
+        return response()->json($filtered->getArray());
     }
 
     /**
-     * should create a new category
+     * should create a new category, but fails if a name is duplicated
      *
-     * @return 200 - category successfully created
+     * @return 201 - category successfully created
      * @return 409 - category already exists
      */
     public function create(Request $request) {
-        // todo janpeer create a category with given information using $response()->all();
+        $params = $request->all();
+        $exist = Category::with('job', 'company')
+            ->where('title', $params['title'])
+            ->get();
+        $existfilter = new Filter($exist, $params);    
+        $existfilter
+            ->byUsedPivots('company')
+            ->byUsedPivots('job');
 
-        $err = false;
+        // check if there are name duplicates in job or company
+        if ($existfilter->isUsedByPivots()) {
+            // if there are already used filters in pivots
+            $existIn = $existfilter
+                ->getUsedPivots('company')
+                ->getUsedPivots('job')
+                ->usedPivots;
 
-        if($err) {
-            abort(409, 'category already exists');
+            return response()->json([
+                'message' => 'Categoryname already exist in the listed job or company',
+                'existIn' => $existIn
+            ], 409); 
+
+        } else if (count($exist) != 0) {
+            return response()->json([
+                    'message' => 'Categoryname already exist'
+                ], 409); 
         }
 
-        return; // returns 200
+        $categories = Category::create($params);
+        $categoriesfilter = new Filter($categories, $params);
+        // save into necessary pivottables if no error
+        $categoriesfilter
+            ->saveToPivot('job')
+            ->saveToPivot('company');
+
+        return response()->json([
+                'message' => 'Category successfully created',
+                'category_id' => $categories->id
+            ], 201);
     }
 
     /**
@@ -71,18 +101,68 @@ class CategoryController extends Controller
      *
      * @return 200 - successfully updated
      * @return 404 - category does not exist
+     * @return 409 - category already exist
     */
     public function update(Request $request, $id) {
-        // todo janpeer update the category information using $response()->all();
-        // todo janpeer check for error 404
+        // todo update pivottables
+        $params = $request->all();
 
-        $err = false;
+        // filter parameters for update pivot
+        $categorypivot = Category::with('job', 'company')->where('id', $id)->get();
+        $filterpivot = new Filter($categorypivot, $params);
+        $pivotparams = $filterpivot
+            ->updatePivot('job')
+            ->updatePivot('company')
+            ->globalParameters;
 
-        if($err) {
-            abort(404, 'category does not exists');
+        // exist filter
+        // todo exlcude itself from search
+        $exist = Category::with('job', 'company')
+            ->where('title', $params['title'])
+            ->get();
+        $existfilter = new Filter($exist, $params);    
+        $existfilter
+            ->byUsedPivots('company')
+            ->byUsedPivots('job');
+
+        // filter for updating
+        $category = Category::find($id);
+        $filter = new Filter($category, $pivotparams);
+
+        if ($category == NULL) {
+            return response()->json([
+                'message' => 'Category does not exist',
+            ], 404);
         }
 
-        return; // returns 200
+        if ($existfilter->isUsedByPivots()) {
+            // if there are already used filters in pivots
+            $existIn = $existfilter
+                ->getUsedPivots('company')
+                ->getUsedPivots('job')
+                ->usedPivots;
+
+            return response()->json([
+                'message' => 'Categoryname already exist in the listed job or company',
+                'existIn' => $existIn
+            ], 409); 
+
+        } else if (count($exist) != 0) {
+            return response()->json([
+                    'message' => 'Categoryname already exist'
+                ], 409); 
+        }
+
+        // todo delete pivots
+        $filter
+            ->saveToPivot('job')
+            ->saveToPivot('company');
+
+        $update = $category->update($params);
+
+        return response()->json([
+                'message' => 'Category successfully updated',
+            ], 200);
     }
 
     /**
@@ -94,13 +174,18 @@ class CategoryController extends Controller
     public function delete(Request $request, $id) {
         // todo janpeer delete the category
         // todo janpeer check for error 404
+        $category = Category::find($id);
 
-        $err = false;
-
-        if($err) {
-            abort(404, 'category does not exists');
+        if ($category == NULL) {
+            return response()->json([
+                'message' => 'Category does not exist',
+            ], 404);
         }
 
-        return; // returns 200
+        $category->delete();
+
+        return response()->json([
+                'message' => 'Category successfully deleted',
+            ], 200);
     }
 }
--- app/Http/Controllers/Filter.php
+++ app/Http/Controllers/Filter.php
@@ -4,28 +4,30 @@ namespace App\Http\Controllers;
 
 class Filter
 {   
-
-    public function __construct($array) {
-        $this->globalarray = $array;
+    /**
+     * @param $array        {Object}    e.g. Category::find(1)->get();
+     * @param $parameters   {Array}     parameters from Illuminate\Http\Request -> e.g. $request->all()
+     */
+    public function __construct($array, $parameters) {
+        $this->globalArray = $array;
+        $this->globalParameters = $parameters;
+        $this->containedPivot = [];
+        $this->usedPivots = [];
     }
+
     /**
      * get a specific eloquent from pivot based on parameters
      *
-     * @param $array        {Object}    e.g. Category::find(1)->get();
-     * @param $parameters   {Array}     parameters from Illuminate\Http\Request -> e.g. $request->all()
-     * @param $keyword      {String}    keyword in the GET request -> ?job[]=2&job[]=1
+     * @param $keyword {String} - keyword in the GET request -> ?job[]=2&job[]=1
      *
-     * @return $array {Array} - filtered array based on parameters
+     * @return $this
      */
-    public function getPivot($parameters, $keyword) {
-        $array = $this->globalarray;
+    public function getPivot($keyword) {
+        $array = $this->globalArray;
+        $parameters = $this->globalParameters;
 
         // no given params will return an empty array
         if (!isset($parameters[$keyword])) {
-            $this->globalarray = $array->reject(function() {
-                return false;
-            });
-
             return $this;
         }
 
@@ -38,35 +40,64 @@ class Filter
                 $filter[] = (int) $id;
             }
 
-            $result = $item->$keyword->whereIn('id', $filter); 
+            $result = $item->$keyword->whereIn('id', $filter);    
 
             return $result;
-        });
+        })
+        ->reject(function($item) {
+            return empty($item);
+        })->values();
 
-        $this->globalarray = $array;
+        $this->globalArray = $array;
         return $this;
     }
 
+    /**
+     * get a specific eloquent from pivot based on parameters
+     *
+     * @param $keyword {String} keyword in the GET request -> ?job[]=2&job[]=1
+     *
+     * @return $this
+     */
+    public function getUsedPivots($keyword) {
+        $cachedArray = $this->globalArray;
+        $parameters = $this->globalParameters;
+
+        $pivots = $this->getPivot($keyword)->getObject();
+
+        // check if there is a pivot table and return the $keywords id
+        foreach ($pivots as $pivot) {
+            foreach($pivot as $item) {
+                if (isset($item['pivot'][$keyword . '_id'])) {
+                    $this->usedPivots[$keyword][] = ($item['pivot'][$keyword . '_id']);
+                }
+            }
+        }
+
+        if (isset($this->usedPivots[$keyword])) {
+            // remove duplicates of $usedPivots
+            $this->usedPivots[$keyword] = array_unique($this->usedPivots[$keyword], SORT_REGULAR);
+        }
+
+        // reset globalArray back to cached array
+        $this->globalArray = $cachedArray;
+        return $this;
+    }
 
     /**
      * filter eloquents based on parameters
      *
-     * @param $array        {Object}    e.g. Category::find(1)->get();
-     * @param $parameters   {Array}     parameters from Illuminate\Http\Request -> e.g. $request->all()
-     * @param $keyword      {String}    keyword in the GET request -> ?job[]=2&job[]=1
+     * @param $keyword {String} keyword in the GET request -> ?job[]=2&job[]=1
      *
-     * @return $array {Array} - filtered array based on parameters
+     * @return $this
      */
-    public function byParameters($parameters, $keyword) {
-        $array = $this->globalarray;
-
+    public function byParameters($keyword) {
+        $array = $this->globalArray;
+        $arraycached = $this->globalArray;
+        $parameters = $this->globalParameters;
 
         // no given params will return an empty array
         if (!isset($parameters[$keyword])) {
-            $this->globalarray = $array->reject(function() {
-                return false;
-            });
-
             return $this;
         }
 
@@ -75,8 +106,106 @@ class Filter
             $filteredParams[] = (int) $id;
         }
 
+        // filter part - filters elements by id in added foreign table named $keyword
+        // e.g. filters Category::with('job')
         $array = $array->map(function($item) use ($filteredParams, $keyword) {
+            if (empty($item)) return [];
 
+            foreach ($item->$keyword as $key) {
+                foreach ($filteredParams as $p) {
+                    if ((int)$key->id === $p) {
+                        return $item;
+                    }
+                }
+            }
+
+            return [];
+        })
+        ->reject(function($item) {
+            return empty($item);
+        })
+        ->values();
+
+        $this->globalArray = $array;
+        return $this;
+    }
+
+    /**
+     * $keyword should be the same name as the method in the specific model with relations
+     *
+     * @param $keyword {String} keyword in the GET request -> ?job[]=2&job[]=1
+     *
+     * @return $this
+     */
+    public function saveToPivot($keyword) {
+        $array = $this->globalArray;
+        $parameters = $this->globalParameters;
+
+        if (!isset($parameters[$keyword])) {
+            return $this;
+        } 
+
+        foreach ($parameters[$keyword] as $p) {
+            $array->$keyword()->attach((int)$p);
+        }
+
+        $this->globalArray = $array;
+        return $this;
+    }
+
+    // todo filter existing pivot to see which is to delete and which to save
+    // todo description
+    public function updatePivot($keyword) {
+        $parameters = $this->globalParameters;
+        // todo filter which pivots exist
+        $idToDelete = $this
+            ->getUsedPivots($keyword)
+            ->usedPivots;
+
+        $idToAdd = array_filter($parameters[$keyword], function ($item) use ($idToDelete) {
+            foreach ($idToDelete as $del) {
+                if ((int)$item == (int)$del) {
+                    return false;
+                }
+            } 
+
+            return true;
+        });
+
+        $this->globalParameters[$keyword] = $idToAdd;
+        $this->globalParameters[$keyword . '_unset'] = $idToDelete[$keyword];
+
+        return $this;
+    }
+
+    //todo deleteFromPivot to delete pivots
+
+    /**
+     * same as byParameters but reset globalArray and
+     * add match into new array $this->containedPivot
+     *
+     * @param $keyword {String} keyword in the GET request -> ?job[]=2&job[]=1
+     *
+     * @return $this
+     */
+    public function byUsedPivots($keyword) {
+        $array = $this->globalArray;
+        $cachedArray = $this->globalArray;
+        $parameters = $this->globalParameters;
+
+        // no given params will return an empty array
+        if (!isset($parameters[$keyword])) {
+            return $this;
+        }
+
+
+        // sort and cast parameters
+        foreach ($parameters[$keyword] as $id) {
+            $filteredParams[] = (int) $id;
+        }
+
+        // sort part - sorts by 
+        $array = $array->map(function($item) use ($filteredParams, $keyword) {
             if (empty($item)) return [];
 
             foreach ($item->$keyword as $key) {
@@ -90,16 +219,49 @@ class Filter
             return [];
         });
 
-        // todo remove empty arrays
-        // problem -> when removing with ->inject then it will change from array to object 
-        $this->globalarray = $array;
+
+        $this->globalArray = $cachedArray;
+        $this->containedPivot[] = $array;
         return $this;
     }
 
     /**
-     * returns the 
+     * usage in combination with byUsedPivots()
+     * checks for found pivot tables
+     *
+     * @return {Boolean} if in $this->containedPivot are values
+     */
+    public function isUsedByPivots() {
+
+        // based on byUsedPivots generated $containedPivot 
+        // it will check if there is a ID found by
+        // the given parameters
+        foreach ($this->containedPivot as $item) {
+            foreach ($item as $i) {
+                if (count($i) === 1) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * returns the array of the Illuminate\Database\Eloquent\Collection object
+     *
+     * @return {Array}
      */
     public function getArray() {
-        return $this->globalarray;
+        return $this->globalArray->toArray();
+    }
+
+    /**
+     * returns full Illuminate\Database\Eloquent\Collection object
+     *
+     * @return Illuminate\Database\Eloquent\Collection {Object}
+     */
+    public function getObject() {
+        return $this->globalArray;
     }
 }
