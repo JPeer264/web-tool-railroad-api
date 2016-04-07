3ae81d3994a887a71f39ec94cc7020adb0fd51f6
jpeer264 <jan.oster94@gmail.com>
2016-04-07 21:29:05 +0200

    added role model

---
+++ app/Role.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Role extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'description', 'ranking'
+    ];
+
+    // LINK THIS MODEL TO OUR DATABASE TABLE ---------------------------------
+    // since the plural of user isnt what we named our database table we have to define it
+    protected $table = 'role';
+
+}
