44b33b9ec99f1b0a963244304d171ee0d77f3a32
jpeer264 <jan.oster94@gmail.com>
2016-04-11 22:52:30 +0200

    updated role seeder

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -12,19 +12,37 @@ class RoleTableSeeder extends Seeder
      */
     public function run()
     {   
+
         $role = DB::table('Role')->get();
         $role_count = count($role);
-        $maximum_count = 5;
+        $maximum_count = 4;
 
-        // prevent that role count is more than 5
+        // prevent that role count is more than 4
         if ($role_count >= $maximum_count) return;
 
-        for ($i = 1; $i <= $maximum_count; $i++) {
-            DB::table('Role')->insert([
-                'name' => str_random(10),
-                'description' => str_random(10),
-                'ranking' => $i
-            ]);
-        }
+        DB::table('Role')->insert([
+            'id' => 1,
+            'name' => 'Superadmin',
+            'description' => 'Have all rights!'
+        ]);
+
+        DB::table('Role')->insert([
+            'id' => 2,
+            'name' => 'Admin',
+            'description' => 'Is not able to delete active users.'
+        ]);
+
+        DB::table('Role')->insert([
+            'id' => 3,
+            'name' => 'Companyadmin',
+            'description' => 'Is able to add and delete members to his own company.'
+        ]);
+
+        DB::table('Role')->insert([
+            'id' => 4,
+            'name' => 'User',
+            'description' => 'Can write topics in categories and comments in topics. Limit on his own job and company'
+        ]);
+
     }
 }
