68630cef4d089d5c98ffccca8004d0345a3e89ce
jpeer264 <jan.oster94@gmail.com>
2016-04-14 17:31:06 +0200

    added test emails with different roles

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -21,13 +21,34 @@ class UserTableSeeder extends Seeder
         $company_count = count($company);
         $company_random = mt_rand(0, $company_count-1);
 
-        $role = DB::table('Role')->get();
-        $role_count = count($role);
-        $role_random = mt_rand(0, $role_count-1);
+        DB::table('User')->insert([
+            'company_id' => $company[$company_random]->id,
+            'role_id' => 1,
+            'job_id' => $job[$job_random]->id,
+            'firstname' => str_random(10),
+            'lastname' => str_random(10),
+            'password' => Hash::make('secret'),
+            'gender' => 'male',
+            'picture_alt' => str_random(10),
+            'picture_location' => '/path/to/picture/',
+            'email' => 'superadmin@gmail.com',
+            'country' => str_random(10),
+            'city' => str_random(10),
+            'address' => str_random(10),
+            'signup_comment' => str_random(100),
+            'birthday' => mt_rand(1000000, 9999999),
+            'Twitter' => 'www.twitter.com',
+            'Facebook' => 'fb.me',
+            'LinkedIn' => 'www.linkedin.com',
+            'Xing' => 'www.xing.com',
+            'accepted' => mt_rand(1000000, 9999999),
+            'accepted_at' => mt_rand(1000000, 9999999),
+            'requested_at' => mt_rand(1000000, 9999999),
+        ]);
 
         DB::table('User')->insert([
             'company_id' => $company[$company_random]->id,
-            'role_id' => $role[$role_random]->id,
+            'role_id' => 2,
             'job_id' => $job[$job_random]->id,
             'firstname' => str_random(10),
             'lastname' => str_random(10),
@@ -52,7 +73,7 @@ class UserTableSeeder extends Seeder
 
         DB::table('User')->insert([
             'company_id' => $company[$company_random]->id,
-            'role_id' => 1,
+            'role_id' => 2,
             'job_id' => $job[$job_random]->id,
             'firstname' => str_random(10),
             'lastname' => str_random(10),
@@ -60,7 +81,32 @@ class UserTableSeeder extends Seeder
             'gender' => 'male',
             'picture_alt' => str_random(10),
             'picture_location' => '/path/to/picture/',
-            'email' => 'superadmin@gmail.com',
+            'email' => 'companyadmin@gmail.com',
+            'country' => str_random(10),
+            'city' => str_random(10),
+            'address' => str_random(10),
+            'signup_comment' => str_random(100),
+            'birthday' => mt_rand(1000000, 9999999),
+            'Twitter' => 'www.twitter.com',
+            'Facebook' => 'fb.me',
+            'LinkedIn' => 'www.linkedin.com',
+            'Xing' => 'www.xing.com',
+            'accepted' => mt_rand(1000000, 9999999),
+            'accepted_at' => mt_rand(1000000, 9999999),
+            'requested_at' => mt_rand(1000000, 9999999),
+        ]);
+
+        DB::table('User')->insert([
+            'company_id' => $company[$company_random]->id,
+            'role_id' => 2,
+            'job_id' => $job[$job_random]->id,
+            'firstname' => str_random(10),
+            'lastname' => str_random(10),
+            'password' => Hash::make('secret'),
+            'gender' => 'male',
+            'picture_alt' => str_random(10),
+            'picture_location' => '/path/to/picture/',
+            'email' => 'user@gmail.com',
             'country' => str_random(10),
             'city' => str_random(10),
             'address' => str_random(10),
