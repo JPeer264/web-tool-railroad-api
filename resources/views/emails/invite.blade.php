49b70bbb662b80e38279398ad4e91ebf502549c7
Hannah93 <Hannelore.hauquier@hotmail.com>
2016-05-12 20:00:34 +0200

    basic layout email

--- resources/views/emails/invite.blade.php
+++ resources/views/emails/invite.blade.php
@@ -1,17 +1,25 @@
 <!DOCTYPE html>
-<html lang="en-US">
+<html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        <meta charset="utf-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Elephorum Email Invite</title>
     </head>
-    <body>
-        <h2>Verify Your Email Address</h2>
+    <body yahoo bgcolor="white">
+        <table width="100%" bgcolor="white" cellpadding="0" cellspacing="0">
+            <tr>
+                <td class="header" bgcolor="#ffc72c" border="0" style="font-size:  24px; border-radius: 0 0 15px ; text-align: center">
+                    Verify Your Email Address
+                </td>
+            </tr>
+            <tr >
+                <td  style="padding: 20px 20px 20px 20px; text-align: center">
+                    You have been invited to join the railway forum.<br>
+                    You can login with your email and the password is "{{$password}}".<br><br>
+                    <a href="{{URL::to('register/invite/' . $invite_expire)}}" style="color:black; background:#ffc72c; padding: 5px 5px 5px 5px; border-radius: 5px 5px 5px 5px">JOIN</a><br/>
 
-        <div>
-            You have been invited to join the railway forum.
-            You can login with your email and the password is {{$password}}.
-            {{ URL::to('register/invite/' . $invite_expire) }}.<br/>
+                </td>
+            </tr>
 
-        </div>
-
+        </table>
     </body>
 </html>
